9141b2e43d287b624364773535c20afec93c1d99
AllanSS2 <allansampa49@gmail.com>
2024-03-19 17:12:33 -0300

    Select no pesquisar projeto e ajustes

--- IntreArquitetura/IntreDesktop/frmPesquisarProjetos.cs
+++ IntreArquitetura/IntreDesktop/frmPesquisarProjetos.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace IntreDesktop
 {
@@ -16,5 +17,31 @@ namespace IntreDesktop
         {
             InitializeComponent();
         }
+
+        //carregar info dos projetos
+
+        public void carregaInfos()
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = "SELECT tbclientes.nomeCli, tipoImovel, tipoServico, status FROM `tbprojetos` inner join tbclientes on tbprojetos.codCli = tbclientes.codCli where status = 1;";
+            comm.CommandType = CommandType.Text;
+
+
+/*
+            comm.Connection = Conexao.obterConexao();
+            MySqlDataReader DR;
+            DR = comm.ExecuteReader();
+            ltbPesquisa.Items.Clear();
+            while (DR.Read())
+            {
+                ltbPesquisa.Items.Add("R$" + DR.GetString(0) + " | " + DR.GetString(1) + " | " + Convert.ToDateTime(DR.GetString(2)).ToShortDateString());
+                total = total + Convert.ToDouble(DR.GetString(0));
+            }
+            txtTotal.Text = Convert.ToString(total);
+
+            Conexao.fecharConexao();*/
+        }
+
+
     }
 }
